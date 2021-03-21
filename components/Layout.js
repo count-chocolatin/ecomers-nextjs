e7b0f2fd40773dd19104178a45c98f45ac0f1c7e
nombregithub <78991960+nombregithub@users.noreply.github.com>
2021-03-21 17:24:54 -0500

    part1

---
+++ components/Layout.js
@@ -0,0 +1,13 @@
+import React, { Children } from 'react'
+import NavBar from './NavBar'
+
+function Layout({children}) {
+    return (
+        <div className="container">
+            <NavBar />
+            {children}
+        </div>
+    )
+}
+
+export default Layout
